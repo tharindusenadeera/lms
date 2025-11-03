fad23d03667dd9d2cfa7b797d1f8dfc9ec16e85e
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2025-11-03 22:46:05 +0000

    shadcn added as the component library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+        <h1 className="text-2xl underline"> Welcome to my SaaS App</h1>
+        <Button>Let's get start</Button>
+    </div>
   )
 }
 
