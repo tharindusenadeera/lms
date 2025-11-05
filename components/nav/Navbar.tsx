cd66f2d19fc3976909726e6929e9d54297e63b8d
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2025-11-05 20:40:32 +0000

    nav item hover feature implemented

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -5,7 +5,7 @@ import NavItems from "@/components/nav/navItems";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <Link href="/public">
+            <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image src="/images/logo.svg" alt="Logo"  width={50} height={50} />
                 </div>
--- components/nav/navItems.tsx
+++ components/nav/navItems.tsx
@@ -1,8 +1,11 @@
-import {useMemo} from "react";
-import {HomeIcon} from "lucide-react";
+'use client'
 import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
 
 const NavItems = () => {
+
+    const pathName = usePathname();
 
     const navItems = [
         {
@@ -13,24 +16,24 @@ const NavItems = () => {
         {
             id: 2,
             title: "Companion",
-            link: 'companion',
+            link: '/companion',
         },
         {
             id: 3,
             title: "My Journey",
-            link: 'profile',
+            link: '/profile',
         },
         {
             id: 4,
             title: "Sign In",
-            link: 'sign-in',
+            link: '/sign-in',
         }
 
     ]
     return (
-        <nav className="flex items-center gap-8">
+        <nav className="flex items-center gap-4">
             {navItems.map(({id, title, link}) => (
-                <Link key={id} href={`/${link}`}>
+                <Link key={id} href={`${link}`} className={cn(pathName === link && "text-primary font-semibold")}>
                     {title}
                 </Link>
             ))}
